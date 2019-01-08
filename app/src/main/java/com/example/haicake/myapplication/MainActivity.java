e1025e642cf565b7fd63390e6c39a571534e7f50
Gia Hai <trangiahai09@gmail.com>
2019-01-08 08:04:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/haicake/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.haicake.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnOK;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnOK = (Button) findViewById(R.id.btnOK);
+
+    }
+}
